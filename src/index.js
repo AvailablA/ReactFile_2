a71364776f581dbbc14c5142180343578b257117
AvailablA <manohar.shewale77@gmail.com>
2023-08-23 12:50:23 +0530

    first commit on project 2

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(<div>
+  <h1>NETFLIX Series</h1>
+  <p> Top 5 web-series</p>
+  <ol>
+    <li>Dark</li>
+    <li>The Lincon Lawyer</li>
+    <li>Jack Rayan</li>
+    <li>Naruto</li>
+    <li>Missin Impossible</li>
+  </ol>
+</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
